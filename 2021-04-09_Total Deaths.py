1ead599f276d13a69a38156fec8ccfb42dedbca5
Blitzbert <pwk286@outlook.com>
2021-04-09 17:28:08 +0200

    analysis of total death by country in Europe and mean of smokers for each country

---
+++ 2021-04-09_Total Deaths.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def read_rawdata (filename):
+    df = pd.read_csv(filename)
+    return df
+
+
+
+
---
+++ 2021-04-09_Total Deaths2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+from 2021-04-09_Total Deaths import read_rawdata
+
+
+
+
